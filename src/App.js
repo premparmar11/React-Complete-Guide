c3665040dbe87c79858abf12962887bd545b7abb
Prem <parmarpremajitsinh@gmail.com>
2020-09-06 06:26:23 +0000

    add new Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
+// JSX code must have 1 parent tag binding whole code
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Prem Here.....</h1>
+      <p>Good Morning</p>
+      <Person />
     </div>
   );
+
+  // ### This is how JSX compiles and converted into like below code
+  //  return React.createElement("div", {className : "App"}, React.createElement("h1", null, "Hello Prem here....."));
+
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Person(){
+    return (
+        <div>
+            <p>
+                I am 21 years Old.
+            </p>
+        </div>
+    );
+};
